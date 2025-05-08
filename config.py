ef690ae1afb35df5507262876e915c881dd479e9
TECHCABZ <113183141+Techcabz@users.noreply.github.com>
2025-05-08 13:16:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class Config:
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() == 'true'
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'false').lower() == 'true'
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'techcabz@gmail.com')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'bjlpviadxlllsqea')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
 
     # Celery Config (using SQLite for broker and backend)
